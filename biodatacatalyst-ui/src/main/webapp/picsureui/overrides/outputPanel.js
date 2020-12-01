03877b10faab7ded50c465a2ca4e3727849e361a
Nathan Chu <chu.nathan@gmail.com>
2020-12-01 18:49:12 -0500

    updates for fixing when the consent values are filtered

--- biodatacatalyst-ui/src/main/webapp/picsureui/overrides/outputPanel.js
+++ biodatacatalyst-ui/src/main/webapp/picsureui/overrides/outputPanel.js
@@ -37,6 +37,49 @@ define(["handlebars", "backbone"], function(HBS, BB){
 		/*
 		 * If you want to show your customized error message, please override this
 		 */
-		outputErrorMessage: undefined
+		outputErrorMessage: undefined,
+		
+		/*
+		 * A function to ensure the correct consents are sent
+		 */
+		updateConsentFilters : function(query, settings) {
+			
+			
+			console.log("update consent filters for query "  + query)
+			
+			parsedSettings = JSON.parse(settings);
+			
+			
+			if(_.filter(_.keys(query.query.categoryFilters), function(concept) {
+				    return concept.includes(parsedSettings.harmonizedPath);
+				}).length == 0 &&  
+				_.filter(_.keys(query.query.numericFilters), function(concept) {
+				    return concept.includes(parsedSettings.harmonizedPath);
+				}).length  == 0 &&  
+				_.filter(query.query.fields, function(concept) {
+				    return concept.includes(parsedSettings.harmonizedPath);
+				}).length  == 0
+			){
+				console.log("removing harmonized consents");
+				query.query.categoryFilters[parsedSettings.harmonizedConsentPath] = undefined;
+			}
+			
+			
+			topmedPresent = false;
+			
+			if(_.keys(query.query.variantInfoFilters[0].numericVariantInfoFilters).length > 0){
+				topmedPresent = true;
+			}
+			
+			if(_.keys(query.query.variantInfoFilters[0].categoryVariantInfoFilters).length > 0){
+				topmedPresent = true;
+			}
+			
+			if(!topmedPresent){
+				console.log("removing Topmed consents");
+				query.query.categoryFilters[parsedSettings.topmedConsentPath] = undefined;
+			}
+			
+	    }
 	};
 });
