595e5cda55fc5c9e7be0b0db028c295b96cb613f
Jason Stedman <namdets@gmail.com>
2021-01-21 16:26:14 -0500

    mapping tmp volume

--- selenium-tests/runchromeheadless.sh
+++ selenium-tests/runchromeheadless.sh
@@ -4,4 +4,4 @@ DOCKER_IMAGE=markhobson/maven-chrome
 
 docker pull $DOCKER_IMAGE
 
-docker run -it -v "$PWD":/usr/src -v "$PWD/tmp":/dev/shm -w /usr/src $DOCKER_IMAGE mvn test -Dpathtoconfigtest="src/test/resources/" -DbrowserName=chromeheadless -Dusername="$1" -Dpassword="$2"
+docker run -it -v "$PWD":/usr/src -v "$PWD"/tmp:/tmp -v "$PWD"/shm:/dev/shm -w /usr/src $DOCKER_IMAGE mvn test -Dpathtoconfigtest="src/test/resources/" -DbrowserName=chromeheadless -Dusername="$1" -Dpassword="$2"
