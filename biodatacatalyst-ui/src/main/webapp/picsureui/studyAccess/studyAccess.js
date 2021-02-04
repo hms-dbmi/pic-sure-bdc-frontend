624c22e34077bf67d47e838ad0c622383a7f4026
Nick Benik <nbenik@gmail.com>
2021-02-04 09:45:50 -0500

    Commit the refactored code

--- biodatacatalyst-ui/src/main/webapp/picsureui/studyAccess/studyAccess.js
+++ biodatacatalyst-ui/src/main/webapp/picsureui/studyAccess/studyAccess.js
@@ -1,3 +1,135 @@
-/**
- * Created by nbeni on 2/4/2021.
- */
+define(["backbone", "handlebars", "text!studyAccess/studyAccess.hbs", "text!studyAccess/studies-data.json", "common/transportErrors"],
+    function(BB, HBS, studyAccessTemplate, studyAccessConfiguration, transportErrors){
+
+        var studyAccess = {};
+
+        studyAccess.studyAccessTemplate = HBS.compile(studyAccessTemplate);
+        studyAccess.configurationData = JSON.parse(studyAccessConfiguration);
+
+        // build the model/collection
+        var studyAccessModel = BB.Model.extend({
+            abbreviated_name: '[N/A]',
+            full_study_name: '[N/A]',
+            clinical_sample_size: 0,
+            clinical_variable_count: 0,
+            consent_group_name: '[N/A]',
+            data_type: '-',
+            genetic_sample_size: 0,
+            study_phase: '-',
+            study_version: '-',
+            request_access: ''
+        });
+        var studyAccessCollection = BB.Collection.extend({
+            model: studyAccessModel,
+            modelId: function(attrs) {
+                return attrs.study_identifier + '.' + attrs.consent_group_code;
+            }
+        });
+
+        // extract the consent identifiers from the query template
+        var session = JSON.parse(sessionStorage.getItem("session"));
+        if (session.queryTemplate === undefined ) {
+            var validConsents = [];
+        } else {
+            var temp = JSON.parse(session.queryTemplate);
+
+            if (temp.categoryFilters === undefined || temp.categoryFilters["\\_consents\\"] === undefined) {
+                var validConsents = [];
+            } else {
+                var validConsents = temp.categoryFilters["\\_consents\\"];
+            }
+        }
+        // process the study data into permission granted or not groups
+        var records = [];
+        for (groupid in studyAccess.configurationData) {
+            for (idx = 0; idx < studyAccess.configurationData[groupid].length; idx++) {
+                // determine if logged in user is permmited access
+                var tmpStudy = studyAccess.configurationData[groupid][idx];
+                tmpStudy["clinical_variable_count"] = parseInt(tmpStudy["clinical_variable_count"]).toLocaleString();
+                tmpStudy["clinical_sample_size"] = parseInt(tmpStudy["clinical_sample_size"]).toLocaleString();
+                tmpStudy["genetic_sample_size"] = parseInt(tmpStudy["genetic_sample_size"]).toLocaleString();
+                var studyConsent = tmpStudy["study_identifier"] + "." + tmpStudy["consent_group_code"]
+                if (validConsents.includes(studyConsent)) {
+                    tmpStudy["disp_group"] = "permitted";
+                } else {
+                    if (tmpStudy["consent_group_code"] == "c0") {
+                        tmpStudy["disp_group"] = "na";
+                    } else {
+                        tmpStudy["disp_group"] = "denied";
+                    }
+                }
+                records.push(tmpStudy);
+            }
+        }
+        // build the collection's models
+        studyAccess.Collection = new studyAccessCollection(records);
+
+        // function for hiding/displaying c0 consents list
+        studyAccess.keystrokeConsentC0 = function(event) {
+        };
+
+        // build view
+        studyAccess.View = new (BB.View.extend({
+            tagName: "div",
+            template: studyAccess.studyAccessTemplate,
+            initialize: function(){},
+            events:{
+                "click .study-lst-btn1": "toggleConsent",
+                "click .study-lst-btn2": "toggleConsent"
+            },
+            toggleConsent: function() {
+                if ($("#no-consent-toggle").hasClass("glyphicon-chevron-down")) {
+                    $(".no-consent-row").show();
+                    $("#no-consent-toggle").removeClass("glyphicon-chevron-down");
+                    $("#no-consent-toggle").addClass("glyphicon-chevron-up");
+                } else {
+                    $(".no-consent-row").hide();
+                    $("#no-consent-toggle").removeClass("glyphicon-chevron-up");
+                    $("#no-consent-toggle").addClass("glyphicon-chevron-down");
+                }
+            },
+            render: function() {
+                var modelData = this.model.toJSON();
+
+                // break into our 3 display groups
+                var outputData = {
+                    permitted: modelData.filter(function(rec) { return rec.disp_group === "permitted"; }),
+                    denied: modelData.filter(function(rec) { return rec.disp_group === "denied"; }),
+                    na: modelData.filter(function(rec) { return rec.disp_group === "na"; })
+                };
+                // sort by "consent group" then "abbreviated name"
+                var funcSort = function (a, b) {
+                    if (a["abbreviated_name"] == b["abbreviated_name"]) {
+                        return (a["consent_group_name"] > b["consent_group_name"]);
+                    } else {
+                        return (a["abbreviated_name"] > b["abbreviated_name"]);
+                    }
+                };
+                outputData.permitted.sort(funcSort);
+                outputData.denied.sort(funcSort);
+                outputData.na.sort(funcSort);
+
+                this.$el.html(studyAccess.studyAccessTemplate(outputData));
+            }
+        }))({
+            model: studyAccess.Collection
+        });
+
+
+        studyAccess.addHeaderTab = function() {
+            // inject button and click handler
+            $('<a class="col-md-2 btn btn-default header-btn" id="data-access-btn" href="#">Data Access</a>').insertAfter("#user-profile-btn");
+            $('#data-access-btn').click(function(e){
+                studyAccess.displayPage();
+                e.currentTarget.blur();
+            }.bind(this));
+        }.bind(studyAccess);
+
+
+        studyAccess.displayPage = function() {
+            studyAccess.View.render();
+            $('#main-content').empty().append(studyAccess.View.$el);
+        };
+
+        return studyAccess;
+    });
