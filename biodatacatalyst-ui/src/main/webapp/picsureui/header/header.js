5f78bd046138f41994e32704a5afb0a0293fa3ad
Ryan Amari <ramari16@gmail.com>
2021-02-09 11:43:13 -0500

    ALS-1641: Move styles from psama to picsure, fix header visibility

--- biodatacatalyst-ui/src/main/webapp/picsureui/header/header.js
+++ biodatacatalyst-ui/src/main/webapp/picsureui/header/header.js
@@ -51,7 +51,7 @@ define(["jquery", "backbone", "handlebars", "studyAccess/studyAccessFunctions", 
             },
             logout : function(event){
                 sessionStorage.clear();
-                window.location = '/psamaui?redirection_url=/picsureui';
+                window.location = '/psamaui/?redirection_url=/picsureui';
             },
             userProfile: function (event) {
                 if(profileOverride.userProfile) {
@@ -120,32 +120,37 @@ define(["jquery", "backbone", "handlebars", "studyAccess/studyAccessFunctions", 
             },
             render : function(){
                 jsonSettings = JSON.parse(settings);
+
                 this.$el.html(this.template({
                     logoPath: (overrides.logoPath
                         ? overrides.logoPath : "/images/logo.png"),
                     helpLink: jsonSettings.helpLink,
                     pdfLink: jsonSettings.pdfLink
                 }));
-                $.ajax({
-                    url: window.location.origin + "/psama/user/me",
-                    type: 'GET',
-                    headers: {"Authorization": "Bearer " + JSON.parse(sessionStorage.getItem("session")).token},
-                    contentType: 'application/json',
-                    success: function(response){
-                        if(response.privileges.includes("ADMIN") || response.privileges.includes("SUPER_ADMIN")){
-                            $('#admin-btn', this.$el).show();
-                        } else {
-                            $('#user-profile-btn', this.$el).show();
-                        }
-                    }.bind(this),
-                    error: function(response){
-                        console.log("error retrieving user info");
-                        console.log(response);
-                    }.bind(this)
-                });
+                if (sessionStorage.getItem("session")) {
+                    $('.authenticated-visible', this.$el).show();
+                    $.ajax({
+                        url: window.location.origin + "/psama/user/me",
+                        type: 'GET',
+                        headers: {"Authorization": "Bearer " + JSON.parse(sessionStorage.getItem("session")).token},
+                        contentType: 'application/json',
+                        success: function (response) {
+                            if(response.privileges.includes("ADMIN")){
+                                $('.admin-visible', this.$el).show();
+                            }
+                            if(response.privileges.includes("SUPER_ADMIN")){
+                                $('.super-admin-visible', this.$el).show();
+                            }
+                        }.bind(this),
+                        error: function (response) {
+                            console.log("error retrieving user info");
+                            console.log(response);
+                        }.bind(this)
+                    });
 
-                // inject data access button (delayed execution)
-                setTimeout(studyAccess.addHeaderTab, 50);
+                    // inject data access button (delayed execution)
+                    setTimeout(studyAccess.addHeaderTab, 50);
+                }
             }
         });
 
