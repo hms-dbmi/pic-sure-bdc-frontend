1ab9afb9896db3eb387c463bf9b3db6e01e53df7
Nathan Chu <chu.nathan@gmail.com>
2021-03-16 16:44:03 -0400

    update sort algorithm for search results

---
+++ biodatacatalyst-ui/src/main/webapp/picsureui/overrides/search.js
@@ -0,0 +1,31 @@
+define([], function(){
+	return {
+		/*
+		 * Override the ordering of search results
+		 */
+		orderResults : function(searchterm, a, b){
+			var aValue = a.value.toLowerCase();
+			var bValue = b.value.toLowerCase();
+			var aOrder = 0;
+			var bOrder = 0;
+			
+			var lastIndexTermA = aValue.lastIndexOf(searchterm);
+			vat lastIndexSeparatorA = aValue.lastIndexOf("\\");
+			
+			while(lastIndexTermA < lastIndexSeparatorA  && lastIndexSeparatorA >= 0){
+				aOrder +=1000;
+				lastIndexSeparatorA = aValue.substring(0, lastIndexSeparatorA).lastIndexOf("\\");
+			}
+			
+			var lastIndexTermB = bValue.lastIndexOf(searchterm);
+			vat lastIndexSeparatorB = bValue.lastIndexOf("\\");
+			while(lastIndexTermB < lastIndexSeparatorB  && lastIndexSeparatorB >= 0){
+				bOrder +=1000;
+				lastIndexSeparatorB = aValue.substring(0, lastIndexSeparatorB).lastIndexOf("\\");
+			}
+				
+	        return aOrder - bOrder;
+	    },
+	
+	};
+});
