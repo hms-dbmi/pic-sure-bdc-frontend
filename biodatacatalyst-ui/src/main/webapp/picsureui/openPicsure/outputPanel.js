9f8c7f001636692b6dba453e68cf21e7500737fc
Nick Benik <nbenik@gmail.com>
2021-03-17 12:05:07 -0400

    Adds updating of study counts, "<10" handling, commas in count values

--- biodatacatalyst-ui/src/main/webapp/picsureui/openPicsure/outputPanel.js
+++ biodatacatalyst-ui/src/main/webapp/picsureui/openPicsure/outputPanel.js
@@ -11,6 +11,7 @@ define(["jquery", "text!../settings/settings.json", "text!openPicsure/outputPane
 		temp.name = x.full_study_name;
 		temp.study_type = x.study_type;
 		temp.request_access = x.request_access;
+		temp.study_concept = "\\_studies\\" + temp.name + " ( " + x.study_identifier + " )\\"; 
 
 		x.study_matches = x.clinical_sample_size.toLocaleString();
 		var t = x.consent_group_name;
@@ -24,7 +25,7 @@ define(["jquery", "text!../settings/settings.json", "text!openPicsure/outputPane
 	});
 	for (var idx in studiesInfo) {
 		studiesInfo[idx].study_matches = studiesInfo[idx].study_matches.toLocaleString(); 
-	}	
+	}
 
 	
 
@@ -100,55 +101,111 @@ define(["jquery", "text!../settings/settings.json", "text!openPicsure/outputPane
 		totalCount: 0,
 		tagName: "div",
 		update: function(incomingQuery){
-			this.model.set("totalPatients",0);
 
+			// clear counts
+			for (var x in studiesInfo) { 
+				studiesInfo[x].study_matches = "--";
+			}
+			this.model.set("totalPatients",0);
 			this.model.spinAll();
 			this.render();
 
-			// make a safe deep copy of the incoming query so we don't modify it
+			// make safe deep copies of the incoming query so we don't modify it
 			var query = JSON.parse(JSON.stringify(incomingQuery));
 			query.resourceCredentials = {};
 			query.query.expectedResultType="COUNT";
 			this.model.set("query", query);
 
-			var dataCallback = function(result){
-				this.model.set("totalPatients", parseInt(result));
-				this.model.set("spinning", false);
-				this.model.set("queryRan", true);
-				this.render();
-			}.bind(this);
-
-			var errorCallback = function(message){
-				this.model.set("spinning", false);
-                                        this.model.set("queryRan", true);
-                                        this.render();
-				$("#patient-count").html(message);
-			}.bind(this);
-
+			// query for total count
 			$.ajax({
 			 	url: window.location.origin + "/picsure/query/sync",
 			 	type: 'POST',
 			 	headers: {"Authorization": "Bearer " + JSON.parse(sessionStorage.getItem("session")).token},
 			 	contentType: 'application/json',
 			 	data: JSON.stringify(query),
-			 	success: function(response){
-			 		dataCallback(response);
-			 	},
-			 	error: function(response){
+			 	success: (function(response){
+					this.model.set("totalPatients", parseInt(response).toLocaleString());
+					this.model.set("spinning", false);
+					this.model.set("queryRan", true);
+					this.render();
+			 	}).bind(this),
+			 	error: (function(response){
 					if (response.status === 401) {
 						history.pushState({}, "", "/psamaui/not_authorized");
 					} else {
-						response.responseText = "<h4>"
+						this.model.set("spinning", false);
+        		                        this.model.set("queryRan", true);
+
+						// handle return of "< 10" response
+						if (response.responseText.indexOf("<") > -1) {
+							this.model.set("totalPatients", response.responseText);
+	               				        this.render();
+						} else {
+							response.responseText = "<h4>"
 							+ overrides.outputErrorMessage ? overrides.outputErrorMessage : "There is something wrong when processing your query, please try it later, if this repeats, please contact admin."
 							+ "</h4>";
-				 		errorCallback(response.responseText);
+	               				        this.render();
+							$("#patient-count").html(response.responseText);
+						}
 					}
-				}
+				}).bind(this)
 			});
+
+
+			// query for the studies counts
+			var queryStudies = JSON.parse(JSON.stringify(incomingQuery));
+			queryStudies.query.crossCountFields = [];
+			for (var x in studiesInfo) { 
+				queryStudies.query.crossCountFields.push(studiesInfo[x].study_concept);
+			}
+			queryStudies.query.expectedResultType="CROSS_COUNT";
+			$.ajax({
+			 	url: window.location.origin + "/picsure/query/sync",
+			 	type: 'POST',
+			 	headers: {"Authorization": "Bearer " + JSON.parse(sessionStorage.getItem("session")).token},
+			 	contentType: 'application/json',
+			 	data: JSON.stringify(queryStudies),
+				success: (function(response) {
+
+					// populate counts and sort
+					var sorted_found = [];
+					var sorted_unfound = [];
+					for (var x in studiesInfo) { 
+						var cnt = response[studiesInfo[x].study_concept];
+						if (cnt.indexOf("<") > -1) {
+							studiesInfo[x].study_matches = cnt;
+							cnt = 1;
+						} else {
+							studiesInfo[x].study_matches = parseInt(cnt).toLocaleString();
+						}
+						if (cnt > 0) {
+							sorted_found.push(studiesInfo[x]);
+						} else {
+							sorted_unfound.push(studiesInfo[x]);
+						}
+					}
+
+					// perform additional sort
+					var sorted_final = sorted_found.concat(sorted_unfound);
+					
+					this.model.set("studies",sorted_final);
+					this.render();
+				}).bind(this),
+				error: (function(response) {
+					for (var x in studiesInfo) { 
+						studiesInfo[x].study_matches = "(error)";
+					}
+					this.render();
+				}).bind(this)
+			});
+
+
 		},
 		render: function(){
 			var context = this.model.toJSON();
-			this.$el.html(this.template(Object.assign({},context , overrides)));
+			// reorder the studies
+			 context.studies
+			this.$el.html(this.template(Object.assign({}, context, overrides)));
 		}
 	});
 	
