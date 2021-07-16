1c6a3744011e28bae5f28a627b139ea729e3c430
Jason Stedman <namdets@gmail.com>
2021-07-16 15:24:42 -0400

    the tags move between required, excluded, inactive. also some styling updates

--- biodatacatalyst-ui/src/main/webapp/picsureui/search-interface/tag-filter-view.js
+++ biodatacatalyst-ui/src/main/webapp/picsureui/search-interface/tag-filter-view.js
@@ -6,10 +6,39 @@ function(BB, HBS, tagFilterViewTemplate){
 
 	let TagFilterModel = BB.Model.extend({
 		defaults:{
-			requiredTags:[],
-			excludedTags:[],
-			hasRequiredTags: false,
-			hasExcludedTags: false
+			requiredTags: new BB.Collection,
+			excludedTags: new BB.Collection,
+			unusedTags: new BB.Collection
+		},
+		initialize: function(opts){
+			this.set('requiredTags', new BB.Collection);
+			this.set('excludedTags', new BB.Collection);
+			this.set('unusedTags', new BB.Collection);
+			HBS.registerHelper("aliasIfStudy", function(tag){
+				if(studyVersionRegex.test(tag)){
+					return findStudyAbbreviationFromId(tag);
+				}
+				return tag;
+			});
+			HBS.registerHelper("colorClass", function(tag){
+				if(studyVersionRegex.test(tag)){
+					return 'study-badge';
+				}
+				return 'tag-badge';
+			});
+			let tagComparator = function(a, b){
+				return b.get('score') - a.get('score');
+			};
+			this.get('requiredTags').comparator = tagComparator;
+			this.get('excludedTags').comparator = tagComparator;
+			this.get('unusedTags').comparator = tagComparator;
+			this.get('unusedTags').add(opts.results.tags);
+		},
+		hasRequiredTags: function(){
+			return this.get('requiredTags').length>0;
+		},
+		hasExcludedTags: function(){
+			return this.get('excludedTags').length>0;
 		}
 	});
 
@@ -17,13 +46,16 @@ function(BB, HBS, tagFilterViewTemplate){
 		initialize: function(opts){
 			this.response = opts.tagSearchResponse;
 			findStudyAbbreviationFromId = opts.findStudyAbbreviationFromId;
-			this.model = new TagFilterModel();
+			this.model = new TagFilterModel(opts.tagSearchResponse);
 		},
 		events: {
 			'mouseover .badge': 'showTagControls',
 			'mouseout .badge': 'hideTagControls',
-			'click .requireTag': 'requireTag',
-			'click .excludeTag': 'excludeTag'
+			'click .require-tag-btn': 'requireTag',
+			'click .exclude-tag-btn': 'excludeTag',
+			'click .remove-required-tag-btn': 'removeRequiredTag',
+			'click .remove-excluded-tag-btn': 'removeExcludedTag',
+			'click .badge': 'clickTag'
 		},
 		showTagControls: function(event){
 			$('.hover-control', event.target).show();
@@ -31,29 +63,77 @@ function(BB, HBS, tagFilterViewTemplate){
 		hideTagControls: function(event){
 			$('.hover-control', event.target).hide();
 		},
-		requireTag: function(event){
-			console.log(event);
+		clickTag: function(event){
+			let tagBtnClicked = this.resolveTagButtonForClick(event);
+			if(tagBtnClicked){
+				this[tagBtnClicked.dataset['action']](tagBtnClicked.dataset['tag']);
+			}
 		},
-		excludeTag: function(event){
-			console.log(event);
+		resolveTagButtonForClick: function(event){
+			let clickIsInsideTagBtn = function(event, tagBtn){
+				let clickXRelativeToTagBtn = (event.offsetX - (tagBtn.offsetLeft - event.target.offsetLeft));
+				return clickXRelativeToTagBtn > 0 && (clickXRelativeToTagBtn - tagBtn.offsetWidth) < tagBtn.offsetWidth;
+			}
+			let tagBtnClicked;
+			_.each($('.hover-control', event.target), tagBtn=>{
+				if(clickIsInsideTagBtn(event, tagBtn)){
+					tagBtnClicked = tagBtn;
+				}
+			});
+			return tagBtnClicked;
+		},
+		requireTag: function(tag){
+			var unusedTags = this.model.get('unusedTags');
+			var requiredTags = this.model.get('requiredTags');
+			var targetTag = unusedTags.findWhere({tag: tag});
+
+			unusedTags.remove(targetTag);
+			requiredTags.add(targetTag);
+			this.render();
+		},
+		excludeTag: function(tag){
+			var unusedTags = this.model.get('unusedTags');
+			var excludedTags = this.model.get('excludedTags');
+			var targetTag = unusedTags.findWhere({tag: tag});
+
+			unusedTags.remove(targetTag);
+			excludedTags.add(targetTag);
+			this.render();
+		},
+		removeRequiredTag: function(tag){
+			var unusedTags = this.model.get('unusedTags');
+			var requiredTags = this.model.get('requiredTags');
+			var targetTag = requiredTags.findWhere({tag: tag});
+
+			requiredTags.remove(targetTag);
+			unusedTags.add(targetTag);
+			this.render();
+		},
+		removeExcludedTag: function(tag){
+			var unusedTags = this.model.get('unusedTags');
+			var excludedTags = this.model.get('excludedTags');
+			var targetTag = excludedTags.findWhere({tag: tag});
+
+			excludedTags.remove(targetTag);
+			unusedTags.add(targetTag);
+			this.render();
 		},
 		render: function(){
+			let unusedTags = this.model.get("unusedTags").toArray();
 			this.$el.html(HBS.compile(tagFilterViewTemplate)(
 				{
-					tags:_.filter(this.response.results.tags, function(tag){return ! studyVersionRegex.test(tag.tag);}),
-					hasRequiredTags:true,
-					hasExcludedTags:true,
-					requiredTags:["cardio"],
-					excludedTags:["plasma"],
-					studyTags:_.map(
-						_.filter(this.response.results.tags, function(tag){
-							return studyRegex.test(tag.tag);
-						}), function(tag){
-							return {
-								study_id: tag,
-								abbreviation: findStudyAbbreviationFromId(tag.tag)
-							};
-						})
+					tags:_.filter(unusedTags, function(tag){
+						return ! studyVersionRegex.test(tag.get('tag'));
+					}).map(function(tag){return tag.toJSON();}),
+					hasRequiredTags:this.model.hasRequiredTags(),
+					hasExcludedTags:this.model.hasExcludedTags(),
+					hasActiveTags: this.model.hasExcludedTags() || this.model.hasRequiredTags(),
+					requiredTags:this.model.get("requiredTags").map(function(tag){return tag.toJSON();}),
+					excludedTags:this.model.get("excludedTags").map(function(tag){return tag.toJSON();}),
+					studyTags:
+						_.filter(unusedTags, function(tag){
+							return studyRegex.test(tag.get('tag'));
+						}).map(function(tag){return tag.toJSON();})
 				})
 			);
 		}
