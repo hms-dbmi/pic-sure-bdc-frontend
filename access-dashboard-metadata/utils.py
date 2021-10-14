2c56c0eaf185441ed9b12a14d323f69c12e59cf5
Emily Hughes <emily_hughes@hms.harvard.edu>
2021-10-14 14:33:25 +0000

    Add function for repetitive code in notebook

--- access-dashboard-metadata/utils.py
+++ access-dashboard-metadata/utils.py
@@ -76,3 +76,38 @@ def joining_variablesDict_onCol(variablesDict: pd.DataFrame,
         .set_index(variablesDict_index_names)
     return variablesDict_joined
 
+def check_new_version(old_file, new_file, columns, old_compare_col = None, new_compare_col = None):
+    #Read in the dataframes
+    old = pd.read_csv(old_file, sep = '\t', skiprows=10)
+    new = pd.read_csv(new_file, sep = '\t', skiprows=10)
+    
+    # Determine which column to use for comparison
+    if old_col is None and new_col is None:
+        print("Finding column to compare...")
+        for col in old.columns:
+            if col.upper() in potential_cols:
+                old_compare_col = col
+            if old_compare_col not in nsm.columns:
+                print("Manual inspection of columns needed")
+            else:
+                new_compare_col = col
+    
+    # Compare the new and old versions of the file            
+    in_new_not_old = list(set(new[new_compare_col])-set(old[old_compare_col]))
+    in_old_not_new = list(set(old[old_compare_col])-set(new[new_compare_col]))
+    
+    # Interpret results
+    if len(in_new_not_old) == 0:
+        print("All", new_compare_col, "in new version are in old")
+        old_diffs = None
+    else:
+        print("There are", len(in_new_not_old), "IDs in new version not in old")
+        old_diffs = old[old[old_compare_col].isin(in_old_not_new)]
+    if len(in_old_not_new) == 0:
+        print("All", old_compare_col, "in old version are in new")
+        new_diffs = None
+    else:
+        print("There are", len(in_old_not_new), "IDs in old version not in new")
+        new_diffs = new[new[compare_col].isin(in_new_not_old)]
+    
+    return(old_diffs, new_diffs)
