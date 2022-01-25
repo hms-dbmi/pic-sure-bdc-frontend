8a7ca2b51fb006254fc3a96e05f824cab88bf441
James <Jamestp19@gmail.com>
2022-01-25 16:29:22 -0500

    ALS-2566: Adds new Profile view (#73)
    * ALS-2555, ALS - 2551: Adds keyboard navigation to the header tabs menu
    
    * adds new dropdown toggle method, fixes focus bugs
    
    * Changes console log to debug
    
    * ALS-2566: New profile view (#72)
    
    * ALS-2566: Creates a view for User Profile using new modal

---
+++ biodatacatalyst-ui/src/main/webapp/picsureui/header/userProfile.js
@@ -0,0 +1,126 @@
+define(["jquery", "backbone","handlebars", "text!header/userProfile.hbs", "picSure/userFunctions"], 
+    function($, BB, HBS, userProfileTemplate, userFunctions){
+    var profileView = BB.View.extend({
+        initialize : function(user){
+            this.template = HBS.compile(userProfileTemplate);
+            this.user = user;
+            HBS.registerHelper('tokenExpiration', function (token) {
+                var expirationTime = JSON.parse(atob(token.split('.')[1])).exp * 1000;
+                var badgeClass = "primary";
+                var badgeMessage = "unknown";
+                var daysLeftOnToken = Math.floor((expirationTime - Date.now()) / (1000 * 60 * 60 * 24));
+                if ( expirationTime < Date.now() ){
+                    badgeClass = "danger";
+                    badgeMessage = "EXPIRED"
+                } else if ( daysLeftOnToken < 7 ) {
+                    badgeClass = "danger";
+                    badgeMessage = "EXPIRING SOON";
+                } else {
+                    badgeClass = "primary";
+                    badgeMessage = "Valid for " + daysLeftOnToken + " more days";
+                }
+                return new Date(expirationTime).toString().substring(0,24) + " <span class='badge badge-" + badgeClass + "'>" + badgeMessage + "</span>";
+            });
+        },
+        events : {
+            'click #user-token-copy-button' : 'copyToken',
+            'click #user-token-refresh-button' : 'refreshToken',
+            'click #user-token-reveal-button' : 'revealToken',
+        },
+        copyToken: function(){
+            $('#user-token-refresh-success').hide();
+            var originValue = document.getElementById("user_token_textarea").textContent;
+
+            var sel = getSelection();
+            var range = document.createRange();
+
+            // this if for supporting chrome, since chrome will look for value instead of textContent
+            // document.getElementById("user_token_textarea").value = document.getElementById("user_token_textarea").textContent;
+            document.getElementById("user_token_textarea").value
+                = document.getElementById("user_token_textarea").textContent
+                = document.getElementById("user_token_textarea").attributes.token.value;
+            range.selectNode(document.getElementById("user_token_textarea"));
+            sel.removeAllRanges();
+            sel.addRange(range);
+            document.execCommand("copy");
+
+            $("#user-token-copy-button").html("Copied");
+
+            document.getElementById("user_token_textarea").textContent
+                = document.getElementById("user_token_textarea").value
+                = originValue;
+        },
+        refreshToken: function(event){
+		    $('#user-token-refresh-button').hide();
+            $('#user-token-refresh-success').hide();
+		    $('#user-token-refresh-confirm-container').show();
+            $('#user-token-yes-button').focus();
+
+            $('#user-token-no-button').click(function(e) {
+                $('#user-token-refresh-confirm-container').hide();
+                $('#user-token-refresh-button').show();
+                $('#user-token-refresh-button').focus();
+                e.preventDefault();
+            });
+		    $('#user-token-yes-button').click(function() {
+                userFunctions.refreshUserLongTermToken(this, function(result){
+                    if ($('#user-token-reveal-button').html() == "Hide"){
+                        $("#user_token_textarea").html(result.userLongTermToken);
+                    }
+
+                    document.getElementById("user_token_textarea").attributes.token.value = result.userLongTermToken;
+                    let newExp = HBS.helpers.tokenExpiration.apply(this, [result.userLongTermToken]);
+                    newExp = 'Token experation time: ' + newExp;
+                    document.getElementById("user_token_expiration").innerHTML = newExp;
+                    $("#user-token-copy-button").html("Copy");
+                    $('#user-token-refresh-confirm-container').hide();
+                    $('#user-token-refresh-success').show();
+                    $('#user-token-refresh-button').show();
+
+                });
+            });
+		    event.preventDefault();
+        },
+        createTabLoop: function(firstFocusableElement, lastFocusableElement) {
+            document.addEventListener('keydown', function(e) {
+                let isTabPressed = e.key === 'Tab' || e.keyCode === 9;
+
+                if (!isTabPressed) {
+                    return;
+                }
+
+                if (e.shiftKey) { // if shift key pressed for shift + tab combination
+                    if ($(document.activeElement).is(firstFocusableElement)) {
+                        lastFocusableElement.focus(); // add focus for the last focusable element
+                        e.preventDefault();
+                    }
+                } else { // if tab key is pressed
+                    if ($(document.activeElement).is(lastFocusableElement)) { // if focused has reached to last focusable element then focus first focusable element after pressing tab
+                        firstFocusableElement.focus(); // add focus for the first focusable element
+                        e.preventDefault();
+                    }
+                }
+            });
+            firstFocusableElement.focus();
+        },
+        revealToken: function(event){
+            $('#user-token-refresh-success').hide();
+            var type = $('#user-token-reveal-button').html();
+            if (type == "Reveal"){
+                var token = $('#user_token_textarea')[0].attributes.token.value;
+                $("#user_token_textarea").html(token);
+                $("#user-token-reveal-button").html("Hide");
+            } else {
+                $("#user_token_textarea").html("**************************************************************************************************************************************************************************************************************************************************************************************");
+                $("#user-token-reveal-button").html("Reveal");
+            }
+        },
+        closeDialog: function () {
+            $("#modalDialog").hide();
+        },
+        render : function(){
+            this.$el.html(this.template({user:this.user}));
+        }
+    });
+    return profileView;
+});
