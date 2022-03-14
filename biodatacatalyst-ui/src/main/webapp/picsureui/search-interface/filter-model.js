bf43a7406f3970475ea1e395c2dcd7449b1edfa8
Sharra Neely <sharra_neely@hms.harvard.edu>
2022-03-14 14:38:22 -0400

    Formatting try 2

--- biodatacatalyst-ui/src/main/webapp/picsureui/search-interface/filter-model.js
+++ biodatacatalyst-ui/src/main/webapp/picsureui/search-interface/filter-model.js
@@ -1,150 +1,142 @@
 define(["backbone", "handlebars"],
-    function (BB, HBS) {
+    function(BB, HBS){
+
         let FilterModel = BB.Model.extend({
-            defaults: {
-                activeFilters: new BB.Collection(),
-                exportFields: new BB.Collection(),
+            defaults:{
+                activeFilters: new BB.Collection,
+                exportFields: new BB.Collection,
             },
-            initialize: function (opts) {
-                this.set("activeFilters", new BB.Collection());
-                this.set("exportFields", new BB.Collection());
-                HBS.registerHelper("filter_type_is", function (type, context) {
-                    return context.type === type;
+            initialize: function(opts){
+                this.set('activeFilters', new BB.Collection);
+                this.set('exportFields', new BB.Collection);
+                HBS.registerHelper("filter_type_is", function(type, context){
+                    return context.type===type;
                 });
             },
-            addCategoryFilter: function (searchResult, values) {
-                let existingFilterForVariable = this.getByVarId(
-                    searchResult.result.varId
-                );
-                if (existingFilterForVariable !== undefined) {
-                    this.get("activeFilters").remove(existingFilterForVariable, {
-                        silent: true,
-                    });
+            addCategoryFilter: function(searchResult, values) {
+                let existingFilterForVariable = this.getByVarId(searchResult.result.varId);
+                if(existingFilterForVariable!==undefined){
+                    this.get('activeFilters').remove(existingFilterForVariable, {silent:true});
                 }
-                this.get("activeFilters").add({
-                    type: "category",
+                this.get('activeFilters').add({
+                    type: 'category',
                     searchResult: searchResult,
                     category: this.generateVariableCategory(searchResult),
                     values: values,
                     searchResult: searchResult,
                     filterType: "restrict",
+                    topmed: searchResult.result.metadata.id.includes('phv'),
                 });
             },
-            addNumericFilter: function (searchResult, min, max) {
-                let existingFilterForVariable = this.getByVarId(
-                    searchResult.result.varId
-                );
-                if (existingFilterForVariable !== undefined) {
-                    this.get("activeFilters").remove(existingFilterForVariable, {
-                        silent: true,
-                    });
+            addNumericFilter: function(searchResult, min, max) {
+                let existingFilterForVariable = this.getByVarId(searchResult.result.varId);
+                if(existingFilterForVariable!==undefined){
+                    this.get('activeFilters').remove(existingFilterForVariable, {silent:true});
                 }
-                this.get("activeFilters").add({
-                    type: "numeric",
+                this.get('activeFilters').add({
+                    type: 'numeric',
                     searchResult: searchResult,
                     category: this.generateVariableCategory(searchResult),
                     min: min,
                     max: max,
-                    filterType:
-                    min === undefined
-                    ? "lessThan"
-                    : max === undefined
-                    ? "greaterThan"
-                    : "between",
+                    filterType: min===undefined ? "lessThan" : max===undefined ? "greaterThan" : "between",
+                    topmed: datatableSelections.searchResult.result.metadata.id.includes('phv'),
                 });
             },
-            addRequiredFilter: function (searchResult) {
-                let existingFilterForVariable = this.getByVarId(
-                    searchResult.result.varId
-                );
-                if (existingFilterForVariable !== undefined) {
-                    this.get("activeFilters").remove(existingFilterForVariable);
+            addRequiredFilter: function(searchResult) {
+                let existingFilterForVariable = this.getByVarId(searchResult.result.varId);
+                if(existingFilterForVariable!==undefined){
+                    this.get('activeFilters').remove(existingFilterForVariable);
                 }
-                this.get("activeFilters").add({
-                    type: "required",
+                this.get('activeFilters').add({
+                    type: 'required',
                     searchResult: searchResult,
                     category: this.generateVariableCategory(searchResult),
                     filterType: "any",
+                    topmed: searchResult.metadata.id.includes('phv'),
                 });
             },
-            addDatatableFilter: function (datatableSelections) {
-                let existingFilterForVariable = this.getByDatatableId(
-                    datatableSelections.searchResult.result.dtId
-                );
-                if (existingFilterForVariable !== undefined) {
-                    this.get("activeFilters").remove(existingFilterForVariable);
+            addDatatableFilter: function(datatableSelections) {
+                let existingFilterForVariable = this.getByDatatableId(datatableSelections.searchResult.result.dtId);
+                if(existingFilterForVariable!==undefined){
+                    this.get('activeFilters').remove(existingFilterForVariable);
                 }
-                this.get("activeFilters").add({
-                    type: "datatable",
+                this.get('activeFilters').add({
+                    type: 'datatable',
                     dtId: datatableSelections.dtId,
                     variables: datatableSelections.variables,
                     category: datatableSelections.title,
                     filterType: "anyRecordOf",
                     datatable: true,
-                    searchResult: datatableSelections.searchResult,
+                    topmed: datatableSelections.searchResult.result.metadata.id.includes('phv'),
+                    searchResult: datatableSelections.searchResult
                 });
             },
-            updateExportField: function (searchResult) {
-                var existingField = this.get("exportFields").find((filter) => {
-                    return filter.get("result").varId === searchResult.result.varId;
+			updateExportField: function (searchResult) {
+				var existingField = this.get("exportFields").find((filter) => {
+					return filter.get("result").varId === searchResult.result.varId;
+				});
+				if (existingField === undefined) {
+					this.addExportField(searchResult);
+				} else {
+					this.removeExportField(existingField);
+				}
+			},
+			isExportField: function (searchResult) {
+				var existingField = this.get("exportFields").find((filter) => {
+					return filter.get("result").varId === searchResult.result.varId;
+				});
+				return existingField !== undefined;
+			},
+			addExportField: function (searchResult) {
+				this.get("exportFields").add(searchResult);
+			},
+			removeExportField: function (searchResult) {
+				this.get("exportFields").remove(searchResult);
+			},
+			getExportFieldCount: function () {
+				return this.get("exportFields").length+4;
+			},
+            addGenomicFilter: function(variantInfoFilters, previousUniqueId = 0) {
+                let existingFilterForGenomic = this.get('activeFilters').find((filter)=>{
+                    return filter.get('type')==='genomic'
+                        // //if we want to allow multiple genomic filters uncomment this line and the one in the genomic filter modal view
+                        //&& filter.get('variantInfoFilters').categoryVariantInfoFilters.__uniqueid === previousUniqueId;
                 });
-                if (existingField === undefined) {
-                    this.addExportField(searchResult);
-                } else {
-                    this.removeExportField(existingField);
+                if(existingFilterForGenomic!==undefined){
+                    this.get('activeFilters').remove(existingFilterForGenomic, {silent:true});
                 }
-            },
-
-            isExportField: function (searchResult) {
-                var existingField = this.get("exportFields").find((filter) => {
-                    return filter.get("result").varId === searchResult.result.varId;
-                });
-                return existingField !== undefined;
-            },
-            addExportField: function (searchResult) {
-                this.get("exportFields").add(searchResult);
-            },
-            removeExportField: function (searchResult) {
-                this.get("exportFields").remove(searchResult);
-            },
-            getExportFieldCount: function () {
-                return this.get("exportFields").length+4;
-            },
-            removeByIndex: function (index) {
-                this.get("activeFilters").remove(this.get("activeFilters").at(index));
-            },
-            getByIndex: function (index) {
-                return this.get("activeFilters").at(index).attributes;
-            },
-            getByVarId: function (varId) {
-                return this.get("activeFilters").find((filter) => {
-                    return filter.get("searchResult").result.varId === varId;
+                this.get('activeFilters').add({
+                    type: 'genomic',
+                    filterType: 'genomic',
+                    genomic: true,
+                    variantInfoFilters: variantInfoFilters,
+                    topmed: false
                 });
             },
-            getByDatatableId: function (dtId) {
-                return this.get("activeFilters").find((filter) => {
-                    return filter.get("dtId") === dtId;
-                });
+            addExportField: function(searchResult){
+                this.get('exportFields').add(searchResult);
             },
-            generateVariableCategory: function (searchResult) {
-                return (
-                    "\\" +
-                    searchResult.result.dtId +
-                    "\\" +
-                    searchResult.result.studyId +
-                    "\\" +
-                    searchResult.result.metadata.varId
-                );
+            removeByIndex: function(index) {
+                this.get('activeFilters').remove(this.get('activeFilters').at(index));
             },
-            generateDatatableCategory: function (searchResult) {
-                return (
-                    "\\" +
-                    searchResult.result.dtId +
-                    "\\" +
-                    searchResult.result.studyId +
-                    "\\"
-                );
+            getByIndex: function(index) {
+                return this.get('activeFilters').at(index).attributes;
+            },
+            getByVarId: function(varId) {
+                return this.get('activeFilters')
+                           .filter((filter) => filter.get('type')!=='genomic')
+                           .find((filter)=>{return filter.get('searchResult').result.varId===varId;});
+            },
+            getByDatatableId: function(dtId) {
+                return this.get('activeFilters').find((filter)=>{return filter.get('dtId')===dtId;});
+            },
+            generateVariableCategory: function(searchResult) {
+                return "\\" + searchResult.result.dtId + "\\" + searchResult.result.studyId + "\\" + searchResult.result.metadata.varId;
+            },
+            generateDatatableCategory: function(searchResult) {
+                return "\\" + searchResult.result.dtId + "\\" + searchResult.result.studyId + "\\";
             },
         });
         return new FilterModel();
-});
+    });
--- biodatacatalyst-ui/src/main/webapp/picsureui/search-interface/variable-info-modal-view.js
+++ biodatacatalyst-ui/src/main/webapp/picsureui/search-interface/variable-info-modal-view.js
@@ -1,222 +1,179 @@
-define([
-    "jquery",
-    "backbone",
-    "handlebars",
-    "text!search-interface/variable-info-modal-template.hbs",
-    "search-interface/tag-filter-model",
-    "text!options/modal.hbs",
-    "search-interface/variable-info-cache",
-    "search-interface/filter-model",
-    "search-interface/categorical-filter-modal-view",
-    "search-interface/numerical-filter-modal-view",
-    "search-interface/datatable-filter-modal-view",
-    "search-interface/modal",
-], function (
-    $,
-    BB,
-    HBS,
-    dataTableInfoTemplate,
-    tagFilterModel,
-    modalTemplate,
-    variableInfoCache,
-    filterModel,
-    categoricalFilterModalView,
-    numericalFilterModalView,
-    datatableFilterModalView,
-    modal
-) {
-    var View = BB.View.extend({
-        initialize: function (opts) {
-            this.dataTableInfoTemplate = HBS.compile(dataTableInfoTemplate);
-            this.modalTemplate = HBS.compile(modalTemplate);
-            this.varId = opts.varId;
-        },
-        events: {
-            "mouseover .badge": "showTagControls",
-            "mouseout .badge": "hideTagControls",
-            "click .require-tag-btn": "requireTag",
-            "click .exclude-tag-btn": "excludeTag",
-            "click .remove-required-tag-btn": "removeRequiredTag",
-            "click .remove-excluded-tag-btn": "removeExcludedTag",
-            "click .badge": "clickTag",
-            "click #show-all-tags-btn": "showAllTags",
-            "click #show-fewer-tags-btn": "showFewerTags",
-            "click .fa-filter": "filterClickHandler",
-            "click .fa-database": "databaseClickHandler",
-            "keypress .fa-filter": "filterKeypressHandler",
-            "keypress .fa-database": "databaseKeypressHandler",
-        },
-        showTagControls: function (event) {
-            $(".hover-control", event.target).show();
-        },
-        hideTagControls: function (event) {
-            $(".hover-control", event.target).hide();
-        },
-        clickTag: function (event) {
-            let tagBtnClicked = this.resolveTagButtonForClick(event);
-            if (tagBtnClicked) {
-                tagFilterModel[tagBtnClicked.dataset["action"]](
-                    tagBtnClicked.dataset["tag"]
-                );
-            }
-        },
-        filterClickHandler: function (event) {
-            let variableId = _.find($(".modal .fa-filter"), (filterButton) => {
-                return filterButton.dataset.target === "variable";
-            }).dataset.id;
+define(["jquery","backbone","handlebars", "text!search-interface/variable-info-modal-template.hbs",
+		"search-interface/tag-filter-model", "text!options/modal.hbs", "search-interface/variable-info-cache",
+		"search-interface/filter-model","search-interface/categorical-filter-modal-view",
+		"search-interface/numerical-filter-modal-view", "search-interface/datatable-filter-modal-view",
+		"search-interface/modal"],
+	function($, BB, HBS, dataTableInfoTemplate,
+			 tagFilterModel, modalTemplate, variableInfoCache,
+			 filterModel, categoricalFilterModalView,
+			 numericalFilterModalView, datatableFilterModalView,
+			 modal){
 
-            let searchResult = _.find(
-                tagFilterModel.attributes.searchResults.results.searchResults,
-                function (variable) {
-                    return variable.result.varId === variableId;
-                }
-            );
+		var View = BB.View.extend({
+			initialize: function(opts){
+				this.dataTableInfoTemplate = HBS.compile(dataTableInfoTemplate);
+				this.modalTemplate = HBS.compile(modalTemplate);
+				this.varId = opts.varId;
+			},
+			events: {
+				'mouseover .badge': 'showTagControls',
+				'mouseout .badge': 'hideTagControls',
+				'click .require-tag-btn': 'requireTag',
+				'click .exclude-tag-btn': 'excludeTag',
+				'click .remove-required-tag-btn': 'removeRequiredTag',
+				'click .remove-excluded-tag-btn': 'removeExcludedTag',
+				'click .badge': 'clickTag',
+				'click #show-all-tags-btn': 'showAllTags',
+				'click #show-fewer-tags-btn': 'showFewerTags',
+				'click .fa-filter': 'filterClickHandler',
+				'click .fa-database': 'databaseClickHandler',
+				'keypress .fa-filter': 'filterKeypressHandler',
+				'keypress .fa-database': 'databaseKeypressHandler'
+			},
+			showTagControls: function(event){
+				$('.hover-control', event.target).show();
+			},
+			hideTagControls: function(event){
+				$('.hover-control', event.target).hide();
+			},
+			clickTag: function(event){
+				let tagBtnClicked = this.resolveTagButtonForClick(event);
+				if(tagBtnClicked){
+					tagFilterModel[tagBtnClicked.dataset['action']](tagBtnClicked.dataset['tag']);
+				}
+			},
+			filterClickHandler: function(event) {
+				let variableId = _.find($('.modal .fa-filter'),
+					(filterButton)=>{return filterButton.dataset.target==='variable';}).dataset.id;
 
-            if (event.target.dataset.target === "variable") {
-                let filter = filterModel.getByVarId(searchResult.result.varId);
+				let searchResult = _.find(tagFilterModel.attributes.searchResults.results.searchResults,
+					function(variable){return variable.result.varId===variableId;});
 
-                let filterViewData = {
-                    searchResult: searchResult,
-                    filter: filter ? filter.toJSON() : undefined,
-                };
+				if(event.target.dataset.target==='variable'){
+					let filter = filterModel.getByVarId(searchResult.result.varId);
 
-                if (!_.isEmpty(searchResult.result.values)) {
-                    this.filterModalView = new categoricalFilterModalView({
-                        data: filterViewData,
-                        el: $(".modal-body"),
-                    });
-                } else {
-                    this.filterModalView = new numericalFilterModalView({
-                        data: filterViewData,
-                        el: $(".modal-body"),
-                    });
-                }
-                this.filterModalView.render();
-                modal.displayModal(
-                    this.filterModalView,
-                    searchResult.result.metadata.description
-                );
-            }
-            if (event.target.dataset.target === "datatable") {
-                let filter = filterModel.getByDatatableId(event.target.dataset.id);
+					let filterViewData = {
+						searchResult: searchResult,
+						filter: filter ? filter.toJSON() : undefined
+					}
 
-                $.ajax({
-                    url:
-                    window.location.origin +
-                    "/picsure/search/36363664-6231-6134-2D38-6538652D3131",
-                    type: "POST",
-                    contentType: "application/json",
-                    data: JSON.stringify({
-                        query: {
-                            searchTerm: "",
-                            includedTags: [event.target.dataset.id],
-                            excludedTags: [],
-                            returnTags: false,
-                            offset: 0,
-                            limit: 100000,
-                        },
-                    }),
-                    success: function (response) {
-                        let filterViewData = {
-                            dtId: event.target.dataset.id,
-                            filter: filter ? filter.toJSON() : undefined,
-                            dtVariables: response.results.searchResults,
-                        };
-                        this.filterModalView = new datatableFilterModalView({
-                            model: filterViewData,
-                            el: $(".modal-body"),
-                        });
-                        this.filterModalView.render();
-                        modal.displayModal(
-                            this.filterModalView,
-                            "Dataset : " + searchResult.result.metadata.dataTableName
-                        );
-                    }.bind(this),
-                    error: function (response) {
-                        console.log(response);
-                    }.bind(this),
-                });
-            }
-        },
-        filterKeypressHandler: function (event) {
-            if (
-                event.key.toLowerCase() === "enter" ||
-                event.key.toLowerCase() === " "
-            ) {
-                this.filterClickHandler(event);
-            }
-        },
-        databaseClickHandler: function (event) {
-            let variableId = _.find($(".modal .fa-filter"), (filterButton) => {
-                return filterButton.dataset.target === "variable";
-            }).dataset.id;
+					if (!_.isEmpty(searchResult.result.values)) {
+						this.filterModalView = new categoricalFilterModalView({
+							data: filterViewData,
+							el: $(".modal-body")
+						});
+					} else {
+						this.filterModalView = new numericalFilterModalView({
+							data: filterViewData,
+							el: $(".modal-body")
+						});
+					}
+					this.filterModalView.render();
+					modal.displayModal(this.filterModalView, searchResult.result.metadata.description);
+				}
+				if(event.target.dataset.target==='datatable'){
+					let filter = filterModel.getByDatatableId(event.target.dataset.id);
 
-            let searchResult = _.find(
-                tagFilterModel.attributes.searchResults.results.searchResults,
-                function (variable) {
-                    return variable.result.varId === event.target.dataset["id"];
-                }
-            );
-            if (event.target.dataset.target === "datatable") {
-                let filter = filterModel.getByVarId(searchResult.result.varId);
+					$.ajax({
+						url: window.location.origin + "/picsure/search/36363664-6231-6134-2D38-6538652D3131",
+						type: 'POST',
+						contentType: 'application/json',
+						data: JSON.stringify({query: {
+								searchTerm: "",
+								includedTags: [event.target.dataset.id],
+								excludedTags: [],
+								returnTags: false,
+								offset: 0,
+								limit: 100000
+						}}),
+						success: function(response){
+							let filterViewData = {
+								dtId: event.target.dataset.id,
+								filter: filter ? filter.toJSON() : undefined,
+								dtVariables: response.results.searchResults
+							};
+							this.filterModalView = new datatableFilterModalView({
+								model: filterViewData,
+								el: $(".modal-body"),
+							});
+							this.filterModalView.render();
+							modal.displayModal(this.filterModalView, "Dataset : " + searchResult.result.metadata.dataTableName);
+						}.bind(this),
+						error: function(response){
+							console.log(response);
+						}.bind(this)
+					});
+				}
+			},
+			filterKeypressHandler: function(event){
+				if(event.key.toLowerCase()==='enter' || event.key.toLowerCase()===' '){
+					this.filterClickHandler(event);
+				}
+			},
+			databaseClickHandler: function(event) {
+				let variableId = _.find($(".modal .fa-filter"), (filterButton) => {
+					return filterButton.dataset.target === "variable";
+				}).dataset.id;
 
-                let filterViewData = {
-                    searchResult: searchResult,
-                    filter: filter ? filter.toJSON() : undefined,
-                };
+				let searchResult = _.find(
+					tagFilterModel.attributes.searchResults.results.searchResults,
+					function (variable) {
+						return variable.result.varId === event.target.dataset["id"];
+					}
+				);
+				if (event.target.dataset.target === "datatable") {
+					let filter = filterModel.getByVarId(searchResult.result.varId);
 
-                if (!_.isEmpty(searchResult.result.values)) {
-                    this.filterModalView = new categoricalFilterModalView({
-                        data: filterViewData,
-                        el: $(".modal-body"),
-                    });
-                } else {
-                    this.filterModalView = new numericalfilterModalView({
-                        data: filterViewData,
-                        el: $(".modal-body"),
-                    });
-                }
-                this.filterModalView.render();
-                modal.displayModal(
-                    this.filterModalView,
-                    searchResult.result.metadata.description
-                );
-            } else {
-                filterModel.updateExportField(searchResult);
-                console.log(
-                    "Current export field count is " + filterModel.getExportFieldCount()
-                );
-            }
-        },
-        databaseKeypressHandler: function (event) {
-            if (
-                event.key.toLowerCase() === "enter" ||
-                event.key.toLowerCase() === " "
-            ) {
-                this.databaseClickHandler(event);
-            }
-        },
-        resolveTagButtonForClick: function (event) {
-            let clickIsInsideTagBtn = function (event, tagBtn) {
-                let clickXRelativeToTagBtn =
-                event.offsetX - (tagBtn.offsetLeft - event.target.offsetLeft);
-                return (
-                    clickXRelativeToTagBtn > 0 &&
-                    clickXRelativeToTagBtn - tagBtn.offsetWidth < tagBtn.offsetWidth
-                );
-            };
-            let tagBtnClicked;
-            _.each($(".hover-control", event.target), (tagBtn) => {
-                if (clickIsInsideTagBtn(event, tagBtn)) {
-                    tagBtnClicked = tagBtn;
-                }
-            });
-            return tagBtnClicked;
-        },
-        render: function () {
-            this.$el.html(this.dataTableInfoTemplate(variableInfoCache[this.varId]));
-        },
-    });
+					let filterViewData = {
+						searchResult: searchResult,
+						filter: filter ? filter.toJSON() : undefined,
+					};
 
-    return View;
-});
+					if (!_.isEmpty(searchResult.result.values)) {
+						this.filterModalView = new categoricalFilterModalView({
+							data: filterViewData,
+							el: $(".modal-body"),
+						});
+					} else {
+						this.filterModalView = new numericalfilterModalView({
+							data: filterViewData,
+							el: $(".modal-body"),
+						});
+					}
+					this.filterModalView.render();
+					modal.displayModal(
+						this.filterModalView,
+						searchResult.result.metadata.description
+					);
+				} else {
+					filterModel.updateExportField(searchResult);
+					console.log(
+						"Current export field count is " + filterModel.getExportFieldCount()
+					);
+				}
+			},
+			databaseKeypressHandler: function(event){
+				if(event.key.toLowerCase()==='enter' || event.key.toLowerCase()===' '){
+					this.filterClickHandler(event);
+				}
+			},
+			resolveTagButtonForClick: function(event){
+				let clickIsInsideTagBtn = function(event, tagBtn){
+					let clickXRelativeToTagBtn = (event.offsetX - (tagBtn.offsetLeft - event.target.offsetLeft));
+					return clickXRelativeToTagBtn > 0 && (clickXRelativeToTagBtn - tagBtn.offsetWidth) < tagBtn.offsetWidth;
+				}
+				let tagBtnClicked;
+				_.each($('.hover-control', event.target), tagBtn=>{
+					if(clickIsInsideTagBtn(event, tagBtn)){
+						tagBtnClicked = tagBtn;
+					}
+				});
+				return tagBtnClicked;
+			},
+			render: function(){
+				this.$el.html(this.dataTableInfoTemplate(variableInfoCache[this.varId]));
+			}
+		});
+
+		return View;
+	});
