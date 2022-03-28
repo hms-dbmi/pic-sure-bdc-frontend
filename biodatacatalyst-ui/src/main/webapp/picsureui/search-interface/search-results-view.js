9a372fd66caa6fbfaefd2cbb755758d4531f3ebc
James <Jamestp19@gmail.com>
2022-03-28 09:26:05 -0400

    Fixes search bugs

--- biodatacatalyst-ui/src/main/webapp/picsureui/search-interface/search-results-view.js
+++ biodatacatalyst-ui/src/main/webapp/picsureui/search-interface/search-results-view.js
@@ -192,7 +192,9 @@ function(BB, HBS, searchResultsViewTemplate, searchResultsListTemplate,
 
 			if (tagFilterModel.get("searchResults")) {
 				if (tagFilterModel.get("searchResults").results.searchResults.length === 0) {
-					$("#search-area").html('<div style="margin-left: 35px" aria-label="0 results match your search">0 results match your search</div>');
+					$('#no-results').length === 0 && $("#search-area").prepend('<div id="no-results" aria-label="0 results match your search">0 results match your search</div>');
+				} else {
+					$('#no-results').remove();
 				}
 				let results = _.map(tagFilterModel.get("searchResults").results.searchResults, function(result, i){
 					let metadata = result.result.metadata;
