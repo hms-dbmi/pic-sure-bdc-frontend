317505df5c124dfc3654997d44b2398601e18c39
James <Jamestp19@gmail.com>
2022-04-07 08:53:57 -0400

    Visualization refactor and updates

--- biodatacatalyst-ui/src/main/webapp/picsureui/search-interface/visualization-modal-view.js
+++ biodatacatalyst-ui/src/main/webapp/picsureui/search-interface/visualization-modal-view.js
@@ -1,10 +1,10 @@
-define(["jquery", "backbone", "handlebars", "text!search-interface/visualization-modal-view.hbs", "search-interface/filter-model", "picSure/queryBuilder", "text!search-interface/visualization-image-partial.hbs", "picSure/settings"],
-function($, BB, HBS, template, filterModel, queryBuilder, imageTemplate, settings) {
+define(["jquery", "backbone", "handlebars", "text!search-interface/visualization-modal-view.hbs", "search-interface/filter-model", "picSure/queryBuilder", "text!search-interface/visualization-image-partial.hbs", "picSure/settings", "common/spinner"],
+function($, BB, HBS, template, filterModel, queryBuilder, imageTemplate, settings, spinner) {
     var visualizationModalView = BB.View.extend({
         initialize: function(opts){
             this.template = HBS.compile(template);
             HBS.registerPartial("visualization-image-partial", imageTemplate);
-            this.images = this.getImages();
+            this.getImages();
             //this.getInfo();
         },
         events: {
@@ -27,22 +27,23 @@ function($, BB, HBS, template, filterModel, queryBuilder, imageTemplate, setting
             let query = queryBuilder.createQueryNew(filterModel.get("activeFilters").toJSON(), "ca0ad4a9-130a-3a8a-ae00-e35b07f1108b");
             query.resourceCredentials = {"Authorization" : "Bearer " + JSON.parse(sessionStorage.getItem("session")).token};
             queryBuilder.updateConsentFilters(query, settings);
+            //$('#images-spinner-holder').html(spinner.getSpinner());
             //let data = JSON.parse(imageJson);
-            let images = null;
-            $.ajax({
+            let deferredResults = $.ajax({
 				url: window.location.origin + '/picsure/query/sync',
 				type: 'POST',
 				contentType: 'application/json',
 				data: JSON.stringify(query),
 				success: function(response){
-                    images = response;
+                    this.images = response;
+                    this.render();
                 }.bind(this),
 				error: function(response){
                     console.error("Viusalzation failed with query: " + JSON.stringify(query));
 					console.error(response);
 				}.bind(this)
 			});
-            return images;
+            spinner.medium(deferredResults, "#images-spinner-holder", "spinner1");
         },
         render: function() {
             this.$el.html(this.template(this.images));
