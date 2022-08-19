fa39ab09641c31f699765133f0462184ed09d18b
James <Jamestp19@gmail.com>
2022-08-19 14:24:02 -0400

    Session override and clean up (#138)
    * Fixes login bug
    
    * Adds session override and cleans up
    
    * simplify loginTemplate

--- biodatacatalyst-ui/src/main/webapp/picsureui/overrides/session.js
+++ biodatacatalyst-ui/src/main/webapp/picsureui/overrides/session.js
@@ -1,5 +1,30 @@
 define([], function(){
     return {
+        handleQueryTemplateAndMeResponseSuccess: function(queryTemplateResponse, meResponse){
+            var currentSession = JSON.parse(sessionStorage.getItem("session"));
+            currentSession.queryTemplate = queryTemplateResponse[0].queryTemplate;
+            currentSession.privileges = meResponse[0].privileges;
+            currentSession.queryScopes = meResponse[0].queryScopes;
+            currentSession.acceptedTOS = meResponse[0].acceptedTOS;
+            currentSession.username = meResponse[0].email;
+
+            if (!currentSession.privileges || currentSession.privileges.length === 0) {
+                history.pushState({}, "", "/picsureui/not_authorized");
+                return;
+            }
+            if (currentSession.privileges && currentSession.privileges.length > 1) {
+                currentSession.privileges.push("FENCE_AUTHORIZED_ACCESS");
+            }
+            
+            sessionStorage.setItem("session", JSON.stringify(currentSession));
+
+            if (sessionStorage.redirection_url && sessionStorage.redirection_url != 'undefined') {
+                window.location = sessionStorage.redirection_url;
+            }
+            else {
+                window.location = "/picsureui/"
+            }
+        },
         handleNotAuthorizedResponse: function() {
             try {
                 if (new Date().getTime()/1000 > JSON.parse(atob(JSON.parse(sessionStorage.session).token.split('.')[1])).exp) {
--- biodatacatalyst-ui/src/main/webapp/psamaui/overrides/fenceLogin.js
+++ biodatacatalyst-ui/src/main/webapp/psamaui/overrides/fenceLogin.js
@@ -2,21 +2,6 @@ define(['picSure/settings', 'jquery', 'handlebars', 'text!login/fence_login.hbs'
         'common/session', 'common/transportErrors', 'util/notification', 'common/searchParser'],
     function(settings, $, HBS, loginTemplate,
              session, transportErrors, notification, searchParser){
-        let loginTemplate = HBS.compile(loginTemplate);
-
-        let sessionInit = function(data) {
-            session.sessionInit(data);
-            let currentSession = JSON.parse(sessionStorage.getItem("session"));
-            if (!currentSession.privileges || currentSession.privileges.length === 0) {
-                history.pushState({}, "", "/picsureui/not_authorized");
-                return;
-            }
-            if (currentSession.privileges && currentSession.privileges.length > 1) {
-                currentSession.privileges.push("FENCE_AUTHORIZED_ACCESS");
-            }
-            sessionStorage.setItem("session", JSON.stringify(currentSession));
-        }
-
         return {
             showLoginPage : function () {
                 var queryObject = searchParser();
@@ -33,7 +18,7 @@ define(['picSure/settings', 'jquery', 'handlebars', 'text!login/fence_login.hbs'
                             code: code
                         }),
                         contentType: 'application/json',
-                        success: sessionInit,
+                        success: session.sessionInit,
                         error: function(data){
                             notification.showFailureMessage("Failed to authenticate with provider. Try again or contact administrator if error persists.")
                             history.pushState({}, "", sessionStorage.not_authorized_url? sessionStorage.not_authorized_url : "/psamaui/not_authorized?redirection_url=/picsureui");
@@ -43,7 +28,7 @@ define(['picSure/settings', 'jquery', 'handlebars', 'text!login/fence_login.hbs'
                     console.log("FENCE-showLoginPage() no code in query string, redirecting to FENCE");
 
                     // Show the fence_login template, with the generated fenceLoginURL
-                    $('#main-content').html(loginTemplate({
+                    $('#main-content').html(HBS.compile(loginTemplate)({
                         fenceURL : settings.idp_provider_uri + "/user/oauth2/authorize"+
                             "?response_type=code"+
                             "&scope=user+openid"+
