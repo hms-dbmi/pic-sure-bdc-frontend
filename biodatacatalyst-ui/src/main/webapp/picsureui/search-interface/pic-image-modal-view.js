43b7c6b3e268cd3adc53737d72c334751cef77ab
James <Jamestp19@gmail.com>
2022-08-31 16:34:14 -0400

    makes code more readable

--- biodatacatalyst-ui/src/main/webapp/picsureui/search-interface/pic-image-modal-view.js
+++ biodatacatalyst-ui/src/main/webapp/picsureui/search-interface/pic-image-modal-view.js
@@ -5,40 +5,39 @@ function($, BB, HBS, template, modal,) {
             this.titles = opts.titles;
             this.images = opts.images;
             this.template = HBS.compile(template);
-            this.location = 0;
-            this.currentImage = this.images[this.location];
-            this.updateButtons();
+            this.imageIndex = 0;
+            this.currentImage = this.images[this.imageIndex];
         },
         events: {
             'click #next-button': 'next',
             'click #prev-button': 'prev',
         },
         next: function() {
-            this.location++;
-            if (this.location>this.images.length-1) {this.location--; return;} 
-            this.currentImage = this.images[this.location];
-            this.updateButtons();
+            this.imageIndex++;
+            if (this.imageIndex>this.images.length-1) {
+                this.imageIndex--; 
+                return;
+            } 
+            this.currentImage = this.images[this.imageIndex];
             this.render();
         },
         prev: function() {
-            this.location--;
-            if (this.location>0) {this.location++; return;}
-            this.currentImage = this.images[this.location];
-            this.updateButtons();
+            this.imageIndex--;
+            if (this.imageIndex>0) {
+                this.imageIndex++; 
+                return;
+            }
+            this.currentImage = this.images[this.imageIndex];
             this.render();
-        },
-        updateButtons: function() {
-            this.disableNext = this.location === this.images.length-1;
-            this.disablePrev = this.location === 0;
         },
         render: function() {
             this.$el.html(this.template({
                 currentImage: this.currentImage,
             }));
             const titles = document.getElementsByClassName('modal-title');
-            if (titles && titles.length>0) {titles[0].innerHTML = this.titles[this.location];}
-            this.$el.find('#next-button').attr('disabled', this.disableNext);
-            this.$el.find('#prev-button').attr('disabled', this.disablePrev);
+            if (titles && titles.length>0) {titles[0].innerHTML = this.titles[this.imageIndex];}
+            this.$el.find('#next-button').attr('disabled', this.imageIndex === this.images.length-1);
+            this.$el.find('#prev-button').attr('disabled', this.imageIndex === 0);
         }
     });
     return ImageModalView;
