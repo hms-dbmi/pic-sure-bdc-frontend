05f425d582c213e5e9b660a9ca843464ab7d2819
Gcolon021 <34667267+Gcolon021@users.noreply.github.com>
2023-04-05 16:00:08 -0400

    Als 4267 bdc security findings click jacking (#186)
    * Update httpd-vhosts.conf
    
    Updated http-vhost.conf to prevent clickjacking attacks. In an attempt to prevent clickjacking we prevent our application from loading in an iFrame.

--- biodatacatalyst-ui/src/main/webapp/picsureui/overrides/header.js
+++ biodatacatalyst-ui/src/main/webapp/picsureui/overrides/header.js
@@ -42,7 +42,7 @@ define([
 	return {
 		/*
 			* The path to a logo image incase you don't want the default PrecisionLink one.
-			* 
+			*
 			* This should be a String value.
 			*/
 		logoPath: undefined,
