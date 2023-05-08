e09196b4e0ded08539dcd95278e93f6bf3f16bb3
gcolon021 <gcolon021@gmail.com>
2023-05-08 15:57:26 -0400

    Update footer redirect modal functionality
    Moved the redirectModal to the global scope to allow other js files to utilize it.

--- biodatacatalyst-ui/src/main/webapp/picsureui/overrides/footer.js
+++ biodatacatalyst-ui/src/main/webapp/picsureui/overrides/footer.js
@@ -1,3 +1,43 @@
+function redirectModal(href, dialog, modal) {
+    let closeModal = () => {
+        $('.close')?.get(0).click();
+    };
+
+    const dialogOption = [
+        {
+            title: "Cancel",
+            "action": () => {
+                closeModal();
+            },
+            classes: "btn btn-default"
+        },
+        {
+            title: "Continue",
+            "action": () => {
+                closeModal();
+                window.open(href, '_blank');
+            },
+            classes: "btn btn-primary"
+        }
+    ];
+    const modalMessage = [
+        "This external website will be opened as a new tab in your browser.",
+        "Are you sure you want to leave BDC-PIC-SURE?"
+    ];
+
+    const dialogView = new dialog({
+        options: dialogOption,
+        messages: modalMessage,
+    });
+    modal.displayModal(
+        dialogView
+        , "Leaving BDC-PIC-SURE"
+        , () => {
+        }
+        , {handleTabs: true, width: "450px"}
+    );
+}
+
 define(["handlebars", "text!overrides/footer.hbs", "common/modal", "common/session", "common/pic-sure-dialog-view", "middleware/middleware"],
     function (HBS, template, modal, session, dialog, Middleware) {
         return {
@@ -31,45 +71,9 @@ define(["handlebars", "text!overrides/footer.hbs", "common/modal", "common/sessi
                 this.$el.html(HBS.compile(template)());
 
                 $(document).on('click', 'a[target="_blank"]', function (event) {
-                        event.preventDefault();
+                    event.preventDefault();
 
-                        let closeModal = () => {
-                            $('.close')?.get(0).click();
-                        };
-
-                        const dialogOption = [
-                            {
-                                title: "Cancel",
-                                "action": () => {
-                                    closeModal();
-                                },
-                                classes: "btn btn-default"
-                            },
-                            {
-                                title: "Continue",
-                                "action": () => {
-                                    closeModal();
-                                    window.open(event.target.href, '_blank');
-                                },
-                                classes: "btn btn-primary"
-                            }
-                        ];
-                        const modalMessage = [
-                            "This external website will be opened as a new tab in your browser.",
-                            "Are you sure you want to leave BDC-PIC-SURE?"
-                        ];
-
-                        const dialogView = new dialog({
-                            options: dialogOption,
-                            messages: modalMessage,
-                        });
-                        modal.displayModal(
-                            dialogView
-                            , "Leaving BDC-PIC-SURE"
-                            , () => {
-                            }
-                            , {handleTabs: true, width: "450px"}
-                        );
+                    redirectModal(event.target.href, dialog, modal);
                 });
             }
         };
