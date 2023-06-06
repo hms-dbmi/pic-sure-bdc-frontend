5a424ce701f5f55c231b657e3e87e75fcd42b0dc
gcolon021 <gcolon021@gmail.com>
2023-06-06 17:08:58 -0400

    Middleware wasn't properly evaluating isOpenAccess

--- biodatacatalyst-ui/src/main/webapp/picsureui/middleware/middleware.js
+++ biodatacatalyst-ui/src/main/webapp/picsureui/middleware/middleware.js
@@ -6,7 +6,7 @@ function (Backbone, settings) {
                 // if url contains picsure
                 if (options.url.indexOf("/picsure") !== -1) {
                     let isOpenAccess = sessionStorage.getItem("isOpenAccess");
-                    let requestSource = isOpenAccess ? 'Open' : 'Authorized';
+                    let requestSource = isOpenAccess === "true" ? 'Open' : 'Authorized';
 
                     jqXHR.setRequestHeader("request-source", requestSource);
                 }
