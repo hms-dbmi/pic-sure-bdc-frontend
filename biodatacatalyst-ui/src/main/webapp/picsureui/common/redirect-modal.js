c7863a991b2956c68cbf8781367ead516f4c43c3
gcolon021 <gcolon021@gmail.com>
2023-06-07 15:46:06 -0400

    Fix for redirect modal
    Redirect modals that were opened and weren't in a modal were having background issues. Fixed that.

--- biodatacatalyst-ui/src/main/webapp/picsureui/common/redirect-modal.js
+++ biodatacatalyst-ui/src/main/webapp/picsureui/common/redirect-modal.js
@@ -1,80 +1,81 @@
 define([
-    "backbone",
-    "jquery",
-    "search-interface/modal",
-    "common/pic-sure-dialog-view"
+        "backbone",
+        "jquery",
+        "search-interface/modal",
+        "common/pic-sure-dialog-view"
     ],
-    function(BB, $, modal, dialog) {
-    return BB.View.extend({
-        initialize: function (opts) {
-            this.data = opts || {};
-        },
-        render: function (sourceEvent, url = undefined) {
-            let closeModal = function () {
-                $('#modal-redirect .close')?.get(0).click();
-            };
+    function (BB, $, modal, dialog) {
+        return BB.View.extend({
+            initialize: function (opts) {
+                this.data = opts || {};
+            },
+            render: function (sourceEvent, url = undefined) {
+                let closeModal = function () {
+                    $('#modal-redirect .close')?.get(0).click();
+                };
 
-            const dialogOption = [
-                {
-                    title: "Cancel",
-                    "action": () => {
-                        closeModal();
+                const dialogOption = [
+                    {
+                        title: "Cancel",
+                        "action": () => {
+                            closeModal();
+                        },
+                        classes: "btn btn-default"
                     },
-                    classes: "btn btn-default"
-                },
-                {
-                    title: "Continue",
-                    "action": () => {
-                        window.open((url === undefined ? sourceEvent.target.href : url), '_blank');
-                        closeModal();
-                    },
-                    classes: "btn btn-primary"
-                }
-            ];
-
-            const modalMessage = [
-                "This external website will be opened as a new tab in your browser.",
-                "Are you sure you want to leave BDC-PIC-SURE?"
-            ];
-
-
-            const modalContainerId = "modal-redirect";
-
-            const dialogView = new dialog({
-                options: dialogOption,
-                messages: modalMessage,
-                modalContainerId: modalContainerId,
-            });
-
-            // Hide the other modal container: modal-window
-            $('#modal-window .modal-dialog').hide();
-
-            modal.displayModal(
-                dialogView
-                , "Leaving BDC-PIC-SURE"
-                , () => { sourceEvent.target.focus(); }
-                , {isHandleTabs: true, width: "450px", modalContainerId: modalContainerId}
-            );
-
-            // Add additional event listener to close button
-            $('#' + modalContainerId + ' #modalDialog').on('click', function(event) {
-                event.preventDefault();
-
-                let parent = $(event.target).parent();
-                // Traverse up the tree until we find the modal-content class or the body
-                while (parent.length && !parent.is('body') && !parent.is('#modalDialog')) {
-                    parent = parent.parent();
-                }
-
-                // If we traversed up to the body, then we clicked outside the modal
-                if (parent.is('#modalDialog')) {
-                    $('#modal-window .modal-dialog').show();
-
-                    if ($('.modal-backdrop in').length === 0) {
-                        $('body').append('<div class="modal-backdrop in"></div>');
+                    {
+                        title: "Continue",
+                        "action": () => {
+                            window.open((url === undefined ? sourceEvent.target.href : url), '_blank');
+                            closeModal();
+                        },
+                        classes: "btn btn-primary"
                     }
-                }
-            });
-        }
+                ];
+
+                const modalMessage = [
+                    "This external website will be opened as a new tab in your browser.",
+                    "Are you sure you want to leave BDC-PIC-SURE?"
+                ];
+
+
+                const modalContainerId = "modal-redirect";
+
+                const dialogView = new dialog({
+                    options: dialogOption,
+                    messages: modalMessage,
+                    modalContainerId: modalContainerId,
+                });
+
+                // Hide the other modal container: modal-window
+                $('#modal-window .modal-dialog').hide();
+
+                modal.displayModal(
+                    dialogView
+                    , "Leaving BDC-PIC-SURE"
+                    , () => {
+                        sourceEvent.target.focus();
+                    }
+                    , {isHandleTabs: true, width: "450px", modalContainerId: modalContainerId}
+                );
+
+                $('#modal-redirect .close').click(function () {
+                    let parent = $(sourceEvent.target).parent();
+                    while (parent.length && !parent.is('body') && !parent.is('.modal-content')) {
+                        parent = parent.parent();
+                    }
+
+                    // If we traversed up to the body, then we clicked outside the modal
+                    if (parent.is('.modal-content')) {
+                        // Add a backdrop to the modal
+                        if ($('.modal-backdrop in').length === 0) {
+                            $('body').append('<div class="modal-backdrop in"></div>');
+                        } else {
+                            $('.modal-backdrop').show();
+                        }
+                    }
+
+                    $('#modal-window .modal-dialog').show();
+                });
+            }
+        });
     });
-});
