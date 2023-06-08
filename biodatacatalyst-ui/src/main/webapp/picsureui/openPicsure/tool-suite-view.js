aa757958b2fe61d4bd3ff8d3e38bb50d2be82c34
gcolon021 <gcolon021@gmail.com>
2023-06-08 12:55:51 -0400

    Add visualization to open tool suite

--- biodatacatalyst-ui/src/main/webapp/picsureui/openPicsure/tool-suite-view.js
+++ biodatacatalyst-ui/src/main/webapp/picsureui/openPicsure/tool-suite-view.js
@@ -1,7 +1,9 @@
 define(["jquery", "backbone", "handlebars", "text!openPicsure/tool-suite-view.hbs",
         "search-interface/modal", "openPicsure/tool-suite-help-view", "openPicsure/outputModel",
-        "openPicsure/studiesModal"],
-    function ($, BB, HBS, template, modal, helpView, outputModel, studiesModal) {
+        "openPicsure/studiesModal", "search-interface/visualization-modal-view", "search-interface/filter-model",
+        "text!openPicsure/visualization-modal-view.hbs"],
+    function ($, BB, HBS, template, modal, helpView, outputModel, studiesModal, VisualizationModalView, filterModel,
+        VisualizationTemplate) {
         return BB.View.extend({
             initialize: function (opts) {
                 this.template = HBS.compile(template);
@@ -12,11 +14,29 @@ define(["jquery", "backbone", "handlebars", "text!openPicsure/tool-suite-view.hb
             events: {
                 'click #participant-study-data': 'openParticipantStudyData',
                 'click #tool-suite-help': 'openHelp',
+                'click #distributions' : 'openDistributions',
                 'keypress #tool-suite-help': 'openHelp',
             },
             handleFilterChange: function () {
+                const filters = filterModel.get('activeFilters');
+                const anyRecordOf = filters.filter(filter => filter.get('filterType') === 'anyRecordOf');
+
                 let shouldDisableParticipantCount = parseInt(outputModel.get("totalPatients")) === 0;
                 this.$el.find('#participant-study-data').prop('disabled', shouldDisableParticipantCount).prop('title', shouldDisableParticipantCount ? 'The "Total Participants" must be greater than zero' : 'Participant Count by Study');
+
+                let shouldDisableDistributions =
+                    parseInt(outputModel.get("totalPatients")) >= 10 && (filters.length + anyRecordOf.length) > 0;
+                this.$el.find('#distributions').prop('disabled', !shouldDisableDistributions).prop('title', shouldDisableDistributions ? 'You must select a filter' : 'Visualize distributions');
+            },
+            openDistributions: function(){
+                // TODO: Update this in the future to use the new VisualizationModalView. For now, we'll just display an empty modal.
+                // const vizModal = new VisualizationModalView.View({model: new VisualizationModalView.Model(), template: VisualizationTemplate});
+                modal.displayModal(
+                    null,
+                    'Variable distributions of query filters',
+                    () => {this.$el.focus();},
+                    {isHandleTabs: true}
+                );
             },
             openHelp: function (event) {
                 if (event.type === "keypress" && !(event.key === ' ' || event.key === 'Enter')) {
