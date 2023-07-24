29e03c7124d1565e41a8a5bbf256acfc13846a9b
Gcolon021 <34667267+Gcolon021@users.noreply.github.com>
2023-07-24 13:54:54 -0400

    Als 4672 (#214)
    Refactor and implement all code necessary for open access statistical visualization.

--- biodatacatalyst-ui/src/main/webapp/picsureui/openPicsure/tool-suite-view.js
+++ biodatacatalyst-ui/src/main/webapp/picsureui/openPicsure/tool-suite-view.js
@@ -1,7 +1,8 @@
 define(["jquery", "backbone", "handlebars", "text!openPicsure/tool-suite-view.hbs",
         "search-interface/modal", "openPicsure/tool-suite-help-view", "openPicsure/outputModel",
-        "openPicsure/studiesModal"],
-    function ($, BB, HBS, template, modal, helpView, outputModel, studiesModal) {
+        "openPicsure/studiesModal","search-interface/visualization-modal-view", "search-interface/filter-model"],
+    function ($, BB, HBS, template, modal, helpView, outputModel, studiesModal, VisualizationModalView,
+              filterModel) {
         return BB.View.extend({
             initialize: function (opts) {
                 this.template = HBS.compile(template);
@@ -12,11 +13,22 @@ define(["jquery", "backbone", "handlebars", "text!openPicsure/tool-suite-view.hb
             events: {
                 'click #participant-study-data': 'openParticipantStudyData',
                 'click #tool-suite-help': 'openHelp',
+                'click #distributions' : 'openDistributions',
                 'keypress #tool-suite-help': 'openHelp',
             },
             handleFilterChange: function () {
-                let shouldDisableParticipantCount = parseInt(outputModel.get("totalPatients")) === 0;
-                this.$el.find('#participant-study-data').prop('disabled', shouldDisableParticipantCount).prop('title', shouldDisableParticipantCount ? 'The "Total Participants" must be greater than zero' : 'Participant Count by Study');
+                let participantCount = parseInt(outputModel.get("totalPatients")) === 0;
+                const filters = filterModel.get('activeFilters');
+                const anyRecordOf = filters.filter(filter => filter.get('filterType') === 'anyRecordOf');
+                const genomic = filters.filter(filter => filter.get('filterType') === 'genomic');
+                let hasPhenotypicFilter = filters.length > 0 && (anyRecordOf.length + genomic.length < filters.length || anyRecordOf.length > 0);
+
+                // Participant Count by Study
+                this.$el.find('#participant-study-data').prop('disabled', participantCount).prop('title', participantCount ? 'The "Total Participants" must be greater than zero' : 'Participant Count by Study');
+
+                // Visualize distributions
+                this.$el.find('#distributions').prop('disabled', participantCount).prop('title', participantCount ? 'The "Total Participants" must be greater than zero' : 'Visualize distributions');
+                this.$el.find('#distributions').prop('disabled', !hasPhenotypicFilter).prop('title', !hasPhenotypicFilter ? 'You must add at least one filter' : 'Visualize distributions');
             },
             openHelp: function (event) {
                 if (event.type === "keypress" && !(event.key === ' ' || event.key === 'Enter')) {
@@ -40,6 +52,15 @@ define(["jquery", "backbone", "handlebars", "text!openPicsure/tool-suite-view.hb
                     {isHandleTabs: true}
                 );
 
+            },
+            openDistributions: function(){
+                const vizModal = new VisualizationModalView.View({model: new VisualizationModalView.Model()});
+                modal.displayModal(
+                    vizModal,
+                    'Variable distributions of query filters',
+                    () => {this.$el.focus();},
+                    {isHandleTabs: true}
+                );
             },
             destroy: function () {
                 //https://stackoverflow.com/questions/6569704/destroy-or-remove-a-view-in-backbone-js/11534056#11534056
--- biodatacatalyst-ui/src/main/webapp/picsureui/search-interface/visualization-modal-view.js
+++ biodatacatalyst-ui/src/main/webapp/picsureui/search-interface/visualization-modal-view.js
@@ -18,7 +18,7 @@ function($, BB, HBS, template, filterModel, queryBuilder, imageTemplate, setting
         modeBarButtonsToRemove: ['pan2d', 'select2d', 'lasso2d', 'zoomIn2d', 'zoomOut2d', 'autoScale2d', 'sendDataToCloud']
     };
     let visualizationModalView = BB.View.extend({
-        initialize: function(){
+        initialize: function(opts){
             this.data = {};
             this.data.traces = [];
             this.data.layouts = [];
@@ -44,10 +44,18 @@ function($, BB, HBS, template, filterModel, queryBuilder, imageTemplate, setting
 				}.bind(this)
 			});
         },
-        getImages: function(){
+    getImages: function(){
             let query = queryBuilder.createQueryNew(filterModel.get("activeFilters").toJSON(), {}, settings.visualizationResourceId);
             query.resourceCredentials = {"Authorization" : "Bearer " + JSON.parse(sessionStorage.getItem("session")).token};
             queryBuilder.updateConsentFilters(query, settings);
+
+            // We need to remove the consent filter for open access as it will reduce the number of results
+            // and we want to show all results for open access. We just obfuscate the data that is not consented to.
+            const isOpenAccess = JSON.parse(sessionStorage.getItem('isOpenAccess'));
+            if (isOpenAccess) {
+                delete query.query.categoryFilters["\\_consents\\"];
+            }
+
             this.model.set('spinning', true);
             $.ajax({
 				url: window.location.origin + '/picsure/query/sync',
@@ -80,18 +88,47 @@ function($, BB, HBS, template, filterModel, queryBuilder, imageTemplate, setting
             }
             return colors;
         },
-        createCategoryPlot: function(){
+        createCategoryPlot: function() {
             this.model.get('categoricalData').forEach((dataMap) => {
+                const obfuscationRange = 6;
+
                 let title = dataMap.title;
                 if (dataMap.title && dataMap.title.length > MAX_TITLE_LENGTH) {
                     title = dataMap.title.substring(0, MAX_TITLE_LENGTH - 3) + "...";
                 }
+
+                let isObfuscated =  dataMap.obfuscated;
                 const values = Object.values(dataMap.categoricalMap);
+
+                // shaded area at top of bar chart
+                const topBar = Array(values.length).fill(obfuscationRange);
+                let traceBottomBarText = [];
+
+                values.forEach((value, i) => {
+                    // If the value is less than 10 and the section obfuscated, then we need to obfuscate the value
+                    if (value < 10 && isObfuscated) {
+                        topBar[i] = 9;
+
+                        // Set the text to < 10 so that it shows up in the bar chart
+                        traceBottomBarText[i] = '< 10';
+
+                        // set the value in the topBar array to 0 so that it doesn't show up in the bar chart
+                        values[i] = 0;
+                    } else if (isObfuscated) {
+                        // The value has been obfuscated by +- obfuscationRange
+                        traceBottomBarText[i] = value + ' \u00B1 3';
+                    } else {
+                        topBar[i] = 0;
+                        traceBottomBarText[i] = value;
+                    }
+                });
+
                 const colors = this.getColors(values.length);
                 const trace = {
                     x: Object.keys(dataMap.categoricalMap),
-                    y: values,
-                    text: values,
+                    // Remove half of the obfuscation range value from the top of the bar
+                    // chart to make room for the shaded area
+                    y: values.map((value, i) => isObfuscated && value > 0 ? value - (obfuscationRange / 2) : value),
                     name: title,
                     unformatedTitle: dataMap.title,
                     type: 'bar',
@@ -99,12 +136,30 @@ function($, BB, HBS, template, filterModel, queryBuilder, imageTemplate, setting
                     isCategorical: true,
                     marker: {
                         color: colors,
-                        line: {
-                            color: 'rgba(0,0,0,0.5)',
-                            width: 2
-                          }
-                    }
+                    },
                 };
+
+                let shadedTrace = {
+                    x: Object.keys(dataMap.categoricalMap),
+                    y: topBar,
+                    name: 'Obfuscated Data',
+                    showlegend: false,
+                    isCategorical: true,
+                    type: 'bar',
+                    marker: {
+                        color: colors,
+                        pattern: {
+                            shape: '/',
+                            size: 20,
+                            solidity: '.5'
+                        }
+                    },
+                    text: traceBottomBarText,
+                    textposition: 'outside',
+                };
+
+                this.data.traces.push([trace, shadedTrace]);
+
                 const layout = {
                     title: title,
                     width: dataMap.chartWidth || 500,
@@ -120,9 +175,10 @@ function($, BB, HBS, template, filterModel, queryBuilder, imageTemplate, setting
                     yaxis: {
                         title: dataMap.yaxisName,
                         automargin: true,
-                    }
-                }
-                this.data.traces.push(trace);
+                    },
+                    barmode: 'stack',
+                };
+
                 this.data.layouts.push(layout);
             });
         },
@@ -132,9 +188,67 @@ function($, BB, HBS, template, filterModel, queryBuilder, imageTemplate, setting
                 if (dataMap.title && dataMap.title.length > MAX_TITLE_LENGTH) {
                     title = dataMap.title.substring(0, MAX_TITLE_LENGTH - 3) + "...";
                 }
+
+                const obfuscationRange = 6;
+                let isObfuscated =  dataMap.obfuscated;
+
+                /*
+                * The dataMap.continuousMap is an object with the key being the range and the value being the count.
+                * When converting the keys, which are a string of the range, to an array of numbers, we are not
+                * guaranteed that the keys will be in order. We need to sort the keys by the lower limit of the range.
+                * If the lower limit is NaN, then it is a single value. We need to sort the keys by the lower limit of
+                * the range.
+                *
+                * This doesn't happen when the data isn't obfuscated because the keys are the same as the values.
+                */
+
+                const keys = Object.keys(dataMap.continuousMap);
+                keys.sort((a, b) => {
+                    // sort the keys by the lower limit of the range
+                    let aLowerLimit = parseFloat(a.split(' -')[0]);
+                    let bLowerLimit = parseFloat(b.split(' -')[0]);
+
+                    // If the lower limit is NaN, then it is a single value
+                    if(isNaN(aLowerLimit)) aLowerLimit = parseFloat(a.split(' +')[0]);
+                    if(isNaN(bLowerLimit)) bLowerLimit = parseFloat(a.split(' +')[0]);
+
+                    return aLowerLimit - bLowerLimit;
+                });
+
+                let orderedKeys = [];
+                let orderedValues = [];
+
+                keys.forEach((key) => {
+                    orderedKeys.push(key);
+                    orderedValues.push(dataMap.continuousMap[key]);
+                });
+
+                // shaded area at top of bar chart
+                const topBar = Array(orderedValues.length).fill(obfuscationRange);
+                let traceBottomBarText = [];
+
+                orderedValues.forEach((value, i) => {
+                    // If the value is less than 10 and the section obfuscated, then we need to obfuscate the value
+                    if (value <= 10 && isObfuscated) {
+                        topBar[i] = 9;
+
+                        // Set the text to < 10 so that it shows up in the bar chart
+                        traceBottomBarText[i] = '< 10';
+
+                        // set the value in the topBar array to 0 so that it doesn't show up in the bar chart
+                        orderedValues[i] = 0;
+                    } else if (isObfuscated) {
+                        // The value has been obfuscated by +- obfuscationRange
+                        traceBottomBarText[i] = value + ' \u00B1 3';
+                    } else {
+                        topBar[i] = 0;
+                        traceBottomBarText[i] = value;
+                    }
+                });
+
                 let trace = {
-                    x: Object.keys(dataMap.continuousMap),
-                    y: Object.values(dataMap.continuousMap),
+                    x: orderedKeys,
+                    y: orderedValues.map((value, i) => isObfuscated && value > 0 ? value - (obfuscationRange / 2) : value),
                     name: title,
                     unformatedTitle: dataMap.title,
                     type: 'bar',
@@ -148,6 +262,32 @@ function($, BB, HBS, template, filterModel, queryBuilder, imageTemplate, setting
                         }
                     }
                 };
+
+                let shadedTrace = {
+                    x: orderedKeys,
+                    y: topBar,
+                    name: 'Obfuscated Data',
+                    showlegend: false,
+                    isCategorical: true,
+                    type: 'bar',
+                    marker: {
+                        color: '#c31f3f',
+                        pattern: {
+                            shape: '/',
+                            size: 20,
+                            solidity: '.5'
+                        },
+                        line: {
+                            color: '#616265',
+                            width: 1
+                        }
+                    },
+                    text: traceBottomBarText,
+                    textposition: 'outside',
+                };
+
+                this.data.traces.push([trace, shadedTrace]);
+
                 let layout = {
                     title: title,
                     width: dataMap.chartWidth || 500,
@@ -165,27 +305,40 @@ function($, BB, HBS, template, filterModel, queryBuilder, imageTemplate, setting
                     yaxis: {
                         title: dataMap.yaxisName,
                         automargin: true,
-                    }
-                }
-                this.data.traces.push(trace);
+                    },
+                    barmode: 'stack',
+                };
+
                 this.data.layouts.push(layout);
             });
         },
         render: function() {
             this.$el.html(this.template(this.model.toJSON()));
-            for(let i = 0; i < this.data.traces.length; i++){
+            for(let i = 0; i < this.data.traces.length; i++) {
                 let plot = document.createElement('div');
                 let screenReaderText = 'Histogram showing the visualization of ';
-                if (this.data.traces[i].isCategorical) {
-                    screenReaderText = 'Column chart showing the visualization of ';
+
+                // We need to do this because the traces are a 2d array. As long as one of the traces is categorical,
+                // we need to add the screen reader text
+                let unformatedTitle;
+                for(let j = 0; j < this.data.traces[i].length; j++) {
+                    if (this.data.traces[i][j].isCategorical) {
+                        screenReaderText = 'Column chart showing the visualization of ';
+                    }
+
+                    if (this.data.traces[i][j].unformatedTitle) {
+                        unformatedTitle = this.data.traces[i][j].unformatedTitle;
+                        break;
+                    }
                 }
+
                 plot.setAttribute('id', 'plot' + i);
-                plot.setAttribute('aria-label', screenReaderText + this.data.traces[i].unformatedTitle);
-                plot.setAttribute('title', 'Visualization of ' + this.data.traces[i].unformatedTitle);
+                plot.setAttribute('aria-label', screenReaderText + unformatedTitle);
+                plot.setAttribute('title', 'Visualization of ' + unformatedTitle);
                 plot.classList.add('image-container');
                 document.getElementById('visualizations-container').appendChild(plot);
-                this.config.toImageButtonOptions.filename = this.data.traces[i].unformatedTitle;
-                plotly.newPlot(plot, [this.data.traces[i]], this.data.layouts[i], this.config);
+                this.config.toImageButtonOptions.filename = unformatedTitle;
+                plotly.newPlot(plot, this.data.traces[i], this.data.layouts[i], this.config);
             }
             return this;
         }
