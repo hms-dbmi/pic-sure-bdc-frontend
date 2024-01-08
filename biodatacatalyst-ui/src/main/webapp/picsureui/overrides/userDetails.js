0e1932fe614f0610b83f397ba589525be331eb26
James <Jamestp19@gmail.com>
2024-01-08 14:21:39 -0500

    Fix missing imports and add better checks

--- biodatacatalyst-ui/src/main/webapp/picsureui/overrides/userDetails.js
+++ biodatacatalyst-ui/src/main/webapp/picsureui/overrides/userDetails.js
@@ -1,31 +1,32 @@
-define([], function(){
+define(["underscore", "picSure/userFunctions"], function(_, userFunctions){
 	return {
 		/*
 		 * Override save user action for BDC specific metadata
          * Instead of replacing the entire user object, just update the roles
 		 */
-		saveUserAction : function(e, model){
+		saveUserAction : function(e, userDetails = {}){
 			e?.preventDefault();
             let user = {};
-			if (model.get("selectedUser") != null && model.get("selectedUser").uuid.trim().length > 0) {
-				user = this.model.get("selectedUser");
+			if (userDetails?.model.get("selectedUser") != null && userDetails?.model.get("selectedUser")?.uuid?.trim().length > 0) {
+				user = userDetails.model.get("selectedUser");
 			}
             let newRoles = [];
-            _.each(this.$('input:checked'), function (checkbox) {
+            _.each(userDetails?.$('input:checked'), function (checkbox) {
 				newRoles.push({uuid: checkbox.value});
 			});
-            if (newRoles === user?.roles) {
+            if (_.isEqual(newRoles, user?.roles)) {
                 console.log("No change to roles");
-                this.render();
-				this.closeDialog();
+                userDetails?.render();
+				userDetails?.closeDialog();
                 return;
             }
-            user?.roles = newRoles;
+            user.roles = newRoles;
+            user.generalMetadata = JSON.stringify(user.generalMetadata); //DB expects string
             userFunctions.createOrUpdateUser([user], user.uuid === null ? 'POST' : 'PUT', function(result) {
 				console.log(result);
-				this.render();
-				this.closeDialog();
-			}.bind(this));
+				userDetails?.render();
+				userDetails?.closeDialog();
+			}.bind(userDetails));
         }	
 	};
 });
